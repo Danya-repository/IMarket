39aaa595d4c49000701621e6f9dc8c6c91c5c501
Danya <d.kashapov@napoleonit.ru>
2021-04-15 23:51:48 +0300

    section banner added

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $(function(){
 
-    
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src ="images/icons/arrow-left.svg" alt = ""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src ="images/icons/arrow-right.svg" alt = ""></button>',
+    });
 
 
 
